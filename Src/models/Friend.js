af20e3a5681e5480cb53e0e515c148a272622c8e
GabboXI <61782098+gabriel-xi@users.noreply.github.com>
2024-12-12 11:41:28 +0100

    Delete Src directory

--- Src/models/Friend.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const FriendSchema = new mongoose.Schema({
-    requester: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    status: { type: String, enum: ['pending', 'accepted', 'declined'], default: 'pending' },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Friend', FriendSchema);
